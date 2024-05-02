192ac63d662baadc74b2983131335d6afb1baece
Bego96 <begic.ismar96@gmail.com>
2024-05-02 20:17:37 +0200

    Set up basic structure for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
 import './App.css';
+import axios, { AxiosResponse } from 'axios';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/navbar';
 import Movies from './pages/movies';
 import Search from './components/search';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+function App(): JSX.Element {
+
   return (
     <div className="App">
       <h1 id="title">Movies and TV Shows Land</h1>
